8b09e21f958bd794b8eadfcda865e49c4ccee093
vedad33 <50501516+vedad33@users.noreply.github.com>
2019-12-12 11:46:31 +0100

    commit16

---
+++ src/labosns/LabosNS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labosns;
+
+/**
+ *
+ * @author wedad
+ */
+public class LabosNS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Glavni g = new Glavni();
+        g.setVisible(true);
+    }
+    
+}
